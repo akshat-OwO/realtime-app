fb13d8ba97e69880e0b8314c7cde8ca3c685d87f
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-07-29 14:38:25 +0530

    loading states and some bug fixes

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -43,7 +43,7 @@ const Messages: FC<MessagesProps> = ({
             pusherClient.unsubscribe(toPusherKey(`chat:${chatId}`));
             pusherClient.unbind('incoming-message', messageHandler);
         };
-    }, []);
+    }, [chatId]);
 
     return (
         <div
